cd951e23878c1e8708b7249c7852da8c3d8890e8
ulancho <ulan.five@gmail.com>
2025-10-18 22:37:07 +0600

    added registration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import Login from "Modules/login";
+
+import Registration from 'Modules/clients/registration';
+import Login from 'Modules/login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/client/registration" element={<Registration />} />
       </Routes>
     </BrowserRouter>
   );
