440ad38d92a459fb2c30fd04e55c67dd98aa76b7
ulancho <ulan.five@gmail.com>
2025-10-23 21:48:42 +0600

    navigate to login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,18 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 
 import Cabinet from 'Common/components/cabinet';
 import Login from 'Modules/login';
 
 function App() {
-  const isLoggedIn = true;
+  const isLoggedIn = false;
 
   return (
     <BrowserRouter>
       {isLoggedIn && <Cabinet />}
       {!isLoggedIn && (
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       )}
     </BrowserRouter>
